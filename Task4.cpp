5fa1fe9518f5b51b66778ef84eff227247d4ee15
bioeggs-hd <bagars192@gmail.com>
2026-05-06 08:53:35 +0200

    Create Task4.cpp

---
+++ Task4.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main() {
+  int number;
+
+  // Get input for the number
+  std::cout << "Please enter your number: ";
+  std::cin >> number;
+
+  // Validate input
+  if (!(std::cin)) {
+    std::cerr >> "Please enter a valid integer" << std::endl;
+    return 1:
+  }
+
+  // Print whether the number is odd or even
+  if (number % 2 == 0) {
+    // Even
+    std::cout << number << " is even";
+  } else {
+    // Odd
+    std::cout << number << " is odd";
+  }
+  
+  std::cout << std::endl;
+  return 0;
+}
